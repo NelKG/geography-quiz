e2bc964e608cc1f6364e008960e041e1dda23aaf
NelKG <>
2023-07-25 16:53:43 +0000

    add style to controls

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,160 +1,160 @@
-const questions = [
-    {
-        question: "What is the capital of Sweden?",
-        answers: [
-            { text: "Oslo", correct: false },
-            { text: "Paris", correct: false },
-            { text: "Stockholm", correct: true },
-            { text: "Cardiff", correct: false },
-        ]
-    },
-    {
-        question: "What is the capital of Thailand?",
-        answers: [
-            { text: "Bangkok", correct: true },
-            { text: "Manila", correct: false },
-            { text: "Dili", correct: false },
-            { text: "Jakarta", correct: false },
-        ]
+// const questions = [
+//     {
+//         question: "What is the capital of Sweden?",
+//         answers: [
+//             { text: "Oslo", correct: false },
+//             { text: "Paris", correct: false },
+//             { text: "Stockholm", correct: true },
+//             { text: "Cardiff", correct: false },
+//         ]
+//     },
+//     {
+//         question: "What is the capital of Thailand?",
+//         answers: [
+//             { text: "Bangkok", correct: true },
+//             { text: "Manila", correct: false },
+//             { text: "Dili", correct: false },
+//             { text: "Jakarta", correct: false },
+//         ]
 
 
 
-    },
-    {
-        question: "What is the capital of Mexico?",
-        answers: [
-            { text: "Caracas", correct: false },
-            { text: "Santiago", correct: false },
-            { text: "Mexico City", correct: true },
-            { text: "Lima", correct: false },
-        ]
+//     },
+//     {
+//         question: "What is the capital of Mexico?",
+//         answers: [
+//             { text: "Caracas", correct: false },
+//             { text: "Santiago", correct: false },
+//             { text: "Mexico City", correct: true },
+//             { text: "Lima", correct: false },
+//         ]
 
-    },
+//     },
 
-    {
-        question: "What is the capital of Malaysia?",
-        answers: [
-            { text: "Beijing", correct: false },
-            { text: "Hong Kong", correct: false },
-            { text: "Kuala Lampur", correct: true },
-            { text: "Kabul", correct: false },
-        ]
-    },
-    {
-        question: "What is the capital of Libya?",
-        answers: [
-            { text: "Addis Ababa", correct: false },
-            { text: "Cape Town", correct: false },
-            { text: "Algiers", correct: false },
-            { text: "Tripoli", correct: true },
-        ]
-    },
-    {
-        question: "What is the capital of Egypt?",
-        answers: [
-            { text: "Dakar", correct: false },
-            { text: "Nairobi", correct: false },
-            { text: "Stockholm", correct: false },
-            { text: "Cairo", correct: true },
-        ]
-    },
+//     {
+//         question: "What is the capital of Malaysia?",
+//         answers: [
+//             { text: "Beijing", correct: false },
+//             { text: "Hong Kong", correct: false },
+//             { text: "Kuala Lampur", correct: true },
+//             { text: "Kabul", correct: false },
+//         ]
+//     },
+//     {
+//         question: "What is the capital of Libya?",
+//         answers: [
+//             { text: "Addis Ababa", correct: false },
+//             { text: "Cape Town", correct: false },
+//             { text: "Algiers", correct: false },
+//             { text: "Tripoli", correct: true },
+//         ]
+//     },
+//     {
+//         question: "What is the capital of Egypt?",
+//         answers: [
+//             { text: "Dakar", correct: false },
+//             { text: "Nairobi", correct: false },
+//             { text: "Stockholm", correct: false },
+//             { text: "Cairo", correct: true },
+//         ]
+//     },
 
-    {
-        question: "What is the capital of Denmark?",
-        answers: [
-            { text: "Budapest", correct: false },
-            { text: "Amsterdam", correct: false },
-            { text: "Copenhagen", correct: true },
-            { text: "Helsinki", correct: false },
-        ]
-    },
-    {
-        question: "What is the capital of Slovenia?",
-        answers: [
-            { text: "Ljublijana", correct: true },
-            { text: "Prague", correct: false },
-            { text: "Lisbon", correct: false },
-            { text: "Moscow", correct: false },
-        ]
-    }
+//     {
+//         question: "What is the capital of Denmark?",
+//         answers: [
+//             { text: "Budapest", correct: false },
+//             { text: "Amsterdam", correct: false },
+//             { text: "Copenhagen", correct: true },
+//             { text: "Helsinki", correct: false },
+//         ]
+//     },
+//     {
+//         question: "What is the capital of Slovenia?",
+//         answers: [
+//             { text: "Ljublijana", correct: true },
+//             { text: "Prague", correct: false },
+//             { text: "Lisbon", correct: false },
+//             { text: "Moscow", correct: false },
+//         ]
+//     }
 
 
 
-];
+// ];
 
-const questionElement = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons");
-const nextButton = document.getElementById("next-btn");
 
-let currentQuestionIndex = 0;
-let score = 0;
+const startButton= document.getElementById('start-btn')
 
-function beginQuiz() {
+startButton.addEventListener('click', startGame)
+// const questionElement = document.getElementById("question");
+// const answerButtons = document.getElementById("answer-buttons");
+// const nextButton = document.getElementById("next-btn");
 
-    currentQuestionIndex = 0;
-    score = 0;
-    nextButton.innerHTML = "Next";
-    showQuestion();
-}
-function showQuestion() {
-    resetState();
-    let currentQuestion = questions[currentQuestionIndex];
-    let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+// let currentQuestionIndex = 0;
+// let score = 0;
 
-    currentQuestion.answers.forEach(answer => {
-        const button = document.createElement("button");
-        button.innerHTML = answer.text;
-        button.classList.add("btn");
-        answerButtons.appendChild(button);
-        if (answer.correct) {
-            button.dataset.correct = answer.correct;
-        }
-        button.addEventListener("click", selectAnswer);
-    });
+function startGame() {
+    console.log('Started')
 }
 
-function resetState() {
+function setNextQuestion(){
 
-    nextButton.style.display = "none";
-    while (answerButtons.firstChild) {
-        answerButtons.removeChild(answerButtons.firstChild);
-    }
 }
 
-function selectAnswer(e) {
-    const selectedBtn = e.target;
-    const isCorrect = selectedBtn.dataset.correct === "true";
-    if (isCorrect) {
-        selectedBtn.classList.add("correct");
-        score++;
-    } else {
-        selectedBtn.classList.add("correct");
-
-    }
-
-    Array.from(answerButtons.children).forEach(button => {
-        if (button.dataset.correct === "true") {
-            button.classList.add("correct");
-        }
-        button.disabled = true;
-    });
-
-    nextButton.style.display = "block";
-}
-
-beginQuiz();
-
-
-
-
-
-
-
-
-
-
-
+function selectAnswer(){
 
 
 }
+//     currentQuestionIndex = 0;
+//     score = 0;
+//     nextButton.innerHTML = "Next";
+//     showQuestion();
+// }
+// function showQuestion() {
+//     resetState();
+//     let currentQuestion = questions[currentQuestionIndex];
+//     let questionNo = currentQuestionIndex + 1;
+//     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+//     currentQuestion.answers.forEach(answer => {
+//         const button = document.createElement("button");
+//         button.innerHTML = answer.text;
+//         button.classList.add("btn");
+//         answerButtons.appendChild(button);
+//         if (answer.correct) {
+//             button.dataset.correct = answer.correct;
+//         }
+//         button.addEventListener("click", selectAnswer);
+//     });
+// }
+
+// function resetState() {
+
+//     nextButton.style.display = "none";
+//     while (answerButtons.firstChild) {
+//         answerButtons.removeChild(answerButtons.firstChild);
+//     }
+// }
+
+// function selectAnswer(e) {
+//     const selectedBtn = e.target;
+//     const isCorrect = selectedBtn.dataset.correct === "true";
+//     if (isCorrect) {
+//         selectedBtn.classList.add("correct");
+//         score++;
+//     } else {
+//         selectedBtn.classList.add("correct");
+
+//     }
+
+//     Array.from(answerButtons.children).forEach(button => {
+//         if (button.dataset.correct === "true") {
+//             button.classList.add("correct");
+//         }
+//         button.disabled = true;
+//     });
+
+//     nextButton.style.display = "block";
+// }
+
+// startQuiz();
